b2988d89e9bdc4939c4e5294bdb8a7265ead6bbe
Stjepan <stipe.milk@gmail.com>
2024-07-30 22:21:39 +0200

    Remove comented-out code

--- app.js
+++ app.js
@@ -12,22 +12,6 @@ app.use(adminRoutes);
 
 app.use(shopRoutes);
 
-// app.use("/add-product", (req, res, next) => {
-//   res.send(
-//     "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
-//   );
-//   res.redirect("/product");
-// });
-
-// app.post("/product", (req, res, next) => {
-//   console.log(req.body);
-//   res.redirect("/");
-// });
-
-// app.use("/", (req, res, next) => {
-//   res.send("<h1>Hello from Express!</h1>");
-// });
-
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found</h1>");
 });
--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-// const bodyParser = require("body-parser");
-
-// const app = express();
-
-// router.use(bodyParser.urlencoded({ extended: false }));
-
 router.get("/add-product", (req, res, next) => {
   res.send(
     "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
