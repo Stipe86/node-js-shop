b3c9b3a200a5f121b94c450c288f31401afeb6ac
Stjepan <stipe.milk@gmail.com>
2024-07-31 00:12:33 +0200

    Serve HTML pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -12,8 +13,12 @@ app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
+// app.use((req, res, next) => {
+//   res.status(404).send("<h1>Page not found</h1>");
+// });
+
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
