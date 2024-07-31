a64e4b6f4953556acfaac89f700f7addf7026971
Stjepan <stipe.milk@gmail.com>
2024-07-31 12:31:41 +0200

    Use a helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 // /admin/add-product => GET
@@ -11,8 +13,13 @@ const router = express.Router();
 //   );
 // });
 
+// /admin/add-product => GET
+// router.get("/add-product", (req, res, next) => {
+//   res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+// });
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
