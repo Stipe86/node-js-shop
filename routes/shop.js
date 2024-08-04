8b845eec3627f7df0c7a5598cf8d2745d0b7400b
Stjepan <stipe.milk@gmail.com>
2024-08-04 13:57:37 +0200

    Add forms.css and update styling; introduce new classes and static product card
    - Add forms.css for form styling
    - Update main.css and product.css and views with new classes
    - Enhance page layout and styling
    - Introduce a static product card in the shop.html, to be dynamically rendered using a templating engine in the next steps

--- routes/shop.js
+++ routes/shop.js
@@ -6,14 +6,6 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
-// router.get("/", (req, res, next) => {
-//   res.send("<h1>Hello from Express!</h1>");
-// });
-
-// router.get("/", (req, res, next) => {
-//   res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
-// });
-
 router.get("/", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
