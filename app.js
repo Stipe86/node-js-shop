d103f6794793223cdeab7436f8785acd86c28d3c
Stjepan <stipe.milk@gmail.com>
2024-10-13 15:59:21 +0200

    Install and set up EJS as the default view engine, create initial '.ejs' templates for page rendering, and update route handlers in controllers to use EJS instead of static HTML files

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+// This line tells Express to use EJS as the templating engine. When you render views in your application, Express will use EJS to generate the HTML
+app.set("view engine", "ejs");
+// This line specifies the directory where your view files (EJS templates) are located. By default, Express looks for the views directory in the root of your project, so if your views are in a directory named "views" at the root level, this line is technically redundant. However, it can be useful for clarity or if you ever change the location of your views
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -19,8 +25,14 @@ app.use(shopRoutes);
 //   res.status(404).send("<h1>Page not found</h1>");
 // });
 
+// Old route handler using static HTML file (before EJS integration). This code was used to serve `404.html` directly from the views folder:
+// app.use((req, res, next) => {
+//   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+// });
+
+// This setup uses EJS as the templating engine to render the `404.ejs` file. Since we set EJS as the default view engine and the `views` folder as the default location, we don’t need to specify the file extension (404.ejs) or the full path to the template (to that folder). Express will automatically look for `404.ejs` in the `views` directory and use it to generate the HTML response
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -8,9 +8,15 @@ const router = express.Router();
 
 const products = [];
 
+// Old route handler using static HTML file (before EJS integration). This code was used to serve `/add-product.html` directly from the views folder:
 // /admin/add-product => GET
+// router.get("/add-product", (req, res, next) => {
+//   res.sendFile(path.join(rootDir, "views", "add-product.html"));
+// });
+
+// This setup uses EJS as the templating engine to render the `add-product.ejs` file. Since we set EJS as the default view engine and the `views` folder as the default location, we don’t need to specify the file extension (add-product.ejs) or the full path to the template (to that folder). Express will automatically look for `add-product.ejs` in the `views` directory and use it to generate the HTML response
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add Product" });
 });
 
 // /admin/add-product => POST
