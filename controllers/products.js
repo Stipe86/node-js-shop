079858dae542c2fdf697e8d9aa6b43d5337b066b
Stjepan <stipe.milk@gmail.com>
2024-10-16 22:07:09 +0200

    Refactor product handling to use Product model
    - Moved product data logic into a new 'Product' model in the 'models/product.js' file
    - Updated controller methods to interact with the model instead of managing data directly
    - Implemented instance method 'save()' and static method 'fetchAll()' in the Product class

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,26 @@
-const products = [];
+// const products = [];
+
+const Product = require("../models/product.js");
+
+// exports.getAddProduct = (req, res, next) => {
+//   res.render("add-product", {
+//     pageTitle: "Add Product",
+//     path: "/admin/add-product",
+//   });
+// };
+
+// exports.postAddProduct = (req, res, next) => {
+//   products.push({ title: req.body.title });
+//   res.redirect("/");
+// };
+
+// exports.getProducts = (req, res, next) => {
+//   res.render("shop", {
+//     pageTitle: "Shop",
+//     products: products,
+//     path: "/",
+//   });
+// };
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -8,16 +30,17 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  // products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  // console.log("shop.js:", adminData.products);
-  // const prods = adminData.products;
+  const prods = Product.fetchAll();
   res.render("shop", {
+    products: prods,
     pageTitle: "Shop",
-    products: products,
     path: "/",
   });
 };
