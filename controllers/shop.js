29c25ca512887efecb57d6d46fdc9fac780c9342
Stjepan <stipe.milk@gmail.com>
2024-10-21 15:39:16 +0200

    Refactor controllers and routes: separate admin and shop logic
    - Added 'admin.js' controller and moved admin-related logic (getAddProduct, postAddProduct) from 'products.js' to 'admin.js'
    - Renamed 'products.js' controller to 'shop.js' to reflect shop-related logic
    - Updated 'admin.js' route file to use 'adminController' instead of 'productsController'
    - Updated 'shop.js' route file to use 'shopController' instead of 'productsController'
    - Adjusted routing files to align with the new controller structure and ensure functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,17 +1,5 @@
 const Product = require("../models/product.js");
 
-exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-};
-
-exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect("/");
-};
 
 exports.getProducts = (req, res, next) => {
   // Passing the anonimous callback function with an argument 'prods' as an argument to fetchAll,
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,10 @@ const path = require("path");
 
 const express = require("express");
 
-const productsController = require("../controllers/products");
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", productsController.getProducts);
+router.get("/", shopController.getProducts);
 
 module.exports = router;
