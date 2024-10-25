fa23bf99c897506cda1622ad513b11fcc6f3000b
Stjepan <stipe.milk@gmail.com>
2024-10-25 22:38:37 +0200

    Expand product model and form to include dynamic properties and UI updates
    - Update Product model constructor to include title, imageUrl, price, and description.
    - Enhance admin add-product controller to handle and save new product properties.
    - Update product-list.ejs to dynamically render product image, price, and description.
    - Adjust add-product form in admin view to accept imageUrl, price, and description inputs.
    - Style textarea in forms.css for consistency with form fields.

--- models/product.js
+++ models/product.js
@@ -36,8 +36,11 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, price, description) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
   }
 
   // Save method to add a new product to the 'products.json' file
