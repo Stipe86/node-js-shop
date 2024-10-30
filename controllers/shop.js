ae40104407f7beb81b0dcabbf56a59d659de16de
Stjepan <stipe.milk@gmail.com>
2024-10-30 11:38:42 +0100

    Add product ID handling for Add to Cart functionality
    - Update form in product-detail.ejs and product-list.ejs to include hidden productId input
    - Add POST route for /cart in shop.js to handle Add to Cart form submissions
    - Implement postCart controller action to temporarily log product ID and redirect to cart page

--- controllers/shop.js
+++ controllers/shop.js
@@ -52,6 +52,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  console.log("Product id: ", productId);
+  res.redirect("/cart");
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     pageTitle: "Your Orders",
