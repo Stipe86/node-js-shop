155e3f834c6cf4bff8c277db9e5f1d4d3043c494
Stjepan <stipe.milk@gmail.com>
2024-11-22 00:58:49 +0100

    Refactor admin controller to use Sequelize for adding products
    - Replace manual product creation logic with Sequelize's 'create()' method.
    - Use 'Product.create()' to insert new product data directly into the database.
    - Add 'then' and 'catch' blocks for handling success and error scenarios.
    
    Note: The response to the client is not sent yet; this will be addressed in a future step.

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,9 +13,19 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, price, description);
-  product.save();
-  res.redirect("/admin/products");
+  Product.create({
+    title: title,
+    imageUrl: imageUrl,
+    price: price,
+    description: description,
+  })
+    .then((result) => {
+      // console.log(result);
+      console.log("Created Product");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
