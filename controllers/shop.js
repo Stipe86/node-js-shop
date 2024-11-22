312044e78d5464286354d08a6d749fe350f41a50
Stjepan <stipe.milk@gmail.com>
2024-11-23 00:24:28 +0100

    Refactor getProduct method and edit styling for product details
    - Refactor getProduct method in shop controller to use Sequelize's findByPk for retrieving a single product by its primary key.
    
    - Update product-detail.ejs to replace the 'card__image' class with a more general 'image' class for better styling consistency.
    
    - Add new styling rules in main.css for the 'image' class to properly display product images with a fixed height and responsive scaling.

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,15 +23,42 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+// exports.getProduct = (req, res, next) => {
+//   const productId = req.params.productId;
+//   Product.findById(productId, (product) => {
+//     res.render("shop/product-detail", {
+//       product: product,
+//       pageTitle: product.title,
+//       path: "/products",
+//     });
+//   });
+// };
+
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
-  Product.findById(productId, (product) => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "/products",
+
+  Product.findByPk(productId)
+    .then((product) => {
+      res.render("shop/product-detail", {
+        product: product,
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
+
+  // An alternative way using findAll
+  // Product.findAll({ where: { id: productId } })
+  //   .then((products) => {
+  //     res.render("shop/product-detail", {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((err) => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
