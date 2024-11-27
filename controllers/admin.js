9e1d898cb8a9540f84bdc2f5855de31a642a9dff
Stjepan <stipe.milk@gmail.com>
2024-11-27 01:11:46 +0100

    Use Magic Association Method for Product Creation
    - Refactor 'postAddProduct' to use the 'createProduct' method from Sequelize’s magic association.
    - Simplify the code by removing the manual 'userId'.
    - Leverage the association between User and Product to improve maintainability and reduce potential errors.

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    imageUrl: imageUrl,
-    price: price,
-    description: description,
-    userId: req.user.id,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      description: description,
+    })
     .then((result) => {
       console.log("CREATED PRODUCT");
       res.redirect("/admin/products");
